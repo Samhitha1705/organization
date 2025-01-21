b1cdcd7061cc6ae542ef0748006f46aef53b7fb8
MTL1016 <vedasamhithagopalam@gmail.com>
2025-01-21 15:03:06 +0530

    first commit

---
+++ src/main/java/com/organizationchart/entity/Employee.java
@@ -0,0 +1,35 @@
+package com.organizationchart.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long sno;
+
+    private Long empId;
+
+    @Column(nullable = false, unique = true)
+    private String email; 
+
+    @Column(nullable = false)
+    private String firstName;
+
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(name = "reportingTo")
+    private Long reportingTo; // Employee ID of the reporting manager
+
+    @Column(nullable = false)
+    private String country; 
+
+    // Getters and Setters
+}
